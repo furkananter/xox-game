c61329f83013c5cd5509f2eed0708988f1ec564b
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-08-29 01:46:59 +0300

    Update Square.js

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 class Square extends React.Component {
+    constructor (props){
+        super(props)
+        this.state = {
+            value : null
+        }
+    }
+
     render() {
         return (
             <button 
-            onClick={() => console.log("clicked")}
+            onClick={() => this.setState({value: 'X'})}
             className="bg-white border-[1px] text-black border-black float-left text-4xl font-bold  -mr-[1px] -mt-[1px] p-0 text-center w-16 h-16 focus:outline-none focus:bg-[#000] focus:text-white">
-                {this.props.value}
+                {this.state.value}
             </button>
         );
     }
