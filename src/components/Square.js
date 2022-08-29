d890416f3141cb375776fe79ebca4f0e3024d3f2
furky.eth <52575351+furkananter@users.noreply.github.com>
2022-08-29 18:52:05 +0300

    Game Completed!

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,22 +1,14 @@
 import React from 'react';
 
-class Square extends React.Component {
-    constructor (props){
-        super(props)
-        this.state = {
-            value : null
-        }
-    }
-
-    render() {
+function Square(props) {
         return (
             <button 
-            onClick={() => this.setState({value: 'X'})}
-            className="bg-white border-[1px] text-black border-black float-left text-4xl font-bold  -mr-[1px] -mt-[1px] p-0 text-center w-16 h-16 focus:outline-none focus:bg-[#000] focus:text-white">
-                {this.state.value}
+            onClick={props.onClick}
+            className="bg-white text-black border-black border-4 float-left text-4xl font-bold  -mr-[1px] -mt-[1px] p-0 text-center w-16 h-16 focus:outline-none focus:bg-[#000] focus:text-white">
+                {props.value}
             </button>
         );
-    }
+    
 }
 
 export default Square
