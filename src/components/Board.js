75e57a4057f65ed8ef27c7fa74d02805b0098491
furky.eth <52575351+furkananter@users.noreply.github.com>
2022-08-30 00:12:36 +0300

    Board.js Comments

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,6 +2,22 @@ import React from 'react';
 import Square from './Square';
 
 
+/* 
+  Square içerisine state eklemektense 
+  Board içerisine bir state eklenip o state üzerinden işlemleri
+  gerçekleştirmek daha mantıklıdır.
+
+  Bu sayede square'ın artık bir class olmasına gerek kalmaz.
+  Fonksiyon olarak bir komponent haline dönüştürüp props ile
+  veri göndermeyi sağlayabiliriz.
+
+*/
+
+// Calculate Winner fonksiyonu:
+// Amaç: tüm kareleri kontrol ederek oyunun kazananını bulmak.
+// Parametre: squares: karelerin dizisi.
+// Potansiyel olarak kazanılan adımları keşfetmek istenmektedir.
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -22,6 +38,12 @@ function calculateWinner(squares) {
   return null
 }
 
+// Board Class'ı:
+// constructor()'ı olan bir sınıf super(props) almak zorunda.
+// this.state ile karelerin durumunu tutan bir state tanımlanır.
+// squares : 9 karelik bir dizi oluşturulur ve her kare için 'null' değerini tutar.
+// xIsNext : X karakteri ilk oynanacak.(true dönmüş) / false olsaydı O karakteri ilk oynanacaktı.
+
 class Board extends React.Component {
   constructor(props) {
     super(props)
@@ -30,6 +52,22 @@ class Board extends React.Component {
       xIsNext: true,
     }
   }
+
+  // handleClick(i) fonksiyonu:
+  // Amaç: kareye tıklandığında o kareye X veya O karakteri yazılır.
+  // Parametre: i: karelerin indexi.
+  // if (calculateWinner(this.state.squares) || this.state.squares[i]) {
+  //   return;
+  // }
+  // bu yukardaki if komutu, eğer işleme giren kare daha önce kullanılmışsa veya oyunun kazananı varsa kareye tıklanamaz.
+  // değiştirilemez.
+  // else altında kareye tıklanıldığında kareye X veya O karakteri yazılır. Sırada kim varsa.
+  // Önemli!
+  // Immutable önemli bir kavramdır. Array'in kendisinin değiştirilmesi tavsiye edilmez. 
+  // Array'in kendisini değiştirmektense .slice() kullanılarak kopyalama yapılır.
+  // const squares = this.state.squares.slice(); 'de olduğu gibi.
+  // squares: squares diyerek kopyalanan dizide değişiklik yapılabilir ve ana diziye en son yapılan değişiklikler gönderilir.
+
 
   handleClick(i) {
     const squares = this.state.squares.slice()
@@ -54,17 +92,33 @@ class Board extends React.Component {
     )
   }
 
+  /*
+    const winner = calculateWinner(this.state.squares)
+    winner adında bir değişken oluşturdum. Bu değişkenimi,
+    calculateWinner fonksiyonunun döndürdüğü değere eşitliyorum.
+    this.state.squares benim kutularımı temsil ediyor.
+    yukarda çalışan fonksiyon squares değerlerini kontrol ediyor.
+    Eğer bu değerler birbirine eşitse, winner değişkeni o değerini
+    döndürüyor.
+    .every() ile squares değerlerini kontrol ediyor.
+    Boş kalan bir değer yoksa ve kazanan yoksa No winner döndürüyor.
+    Else zaten görüldüğü gibi, boş kalan varsa kimin oynayacağını söylüyor.
+  
+  */
   render() {
     const winner = calculateWinner(this.state.squares)
     let status;
     if (winner) {
       status = "Winner: " + winner
-    } else if(this.state.squares.every(square => square !== null)) {
+    } else if (this.state.squares.every(square => square !== null)) {
       status = "No Winner"
-    }else{
+    } else {
       status = "Next Player: " + (this.state.xIsNext ? 'X' : 'O')
     }
 
+
+    // Framer Motion Part
+    // Açılış kısmındaki animasyon.
     const item = {
       hidden: { y: 20, opacity: 0 },
       visible: {
@@ -72,29 +126,32 @@ class Board extends React.Component {
         opacity: 1
       }
     };
+    // Framer Motion Part
 
     return (
-      <div 
-      initial="hidden"
-      animate="visible"
-      variants={item}>
-        <div className="mb-2.5">{status}</div>
+      <div
+        // Başlangıçta Hidden, sonra visible.
+        initial="hidden"
+        animate="visible"
+        variants={item}
+      >
+        <div className="mb-2.5">{status}</div>{/* Status Div */}
         <div className="after:content-[''] table-auto clear-both">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
-        </div>
+        </div>{/* 1th div */}
         <div className="table-auto after:content-[''] clear-both">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
-        </div>
+        </div>{/* 2th div */}
         <div className="table-auto after:content-[''] clear-both">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
-        </div>
-      </div>
+        </div>{/* 3th div */}
+      </div>/* Wrapper */
     );
   }
 }
