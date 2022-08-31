c04253d4a92f1a2c488a306e3549f38c05c02f8a
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-08-31 22:13:27 +0300

    Game Style

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import Board from './components/Board'
 
+
+const style = {
+  main: "flex w-full h-screen py-10 m-auto bg-black text-white text-center justify-center",
+  board: "justify-center items-center flex",
+  statusAndTodo: "ml-5",
+  orderedList:"pl-8"
+}
 // Game Class'ı:
 // App.js gibi düşünülebilir.
 // Component'lerin renderlandığı yer.
@@ -8,15 +15,15 @@ import Board from './components/Board'
 class Game extends React.Component {
   render() {
     return (
-      <div className="flex w-full h-screen py-10 m-auto bg-black text-white text-center justify-center">
-        <div className='justify-center items-center flex'>
+      <div className={style.main}>
+        <div className={style.board}>
           <Board />
         </div>{/* Board div */}
-        <div className="ml-5">
+        <div className={style.statusAndTodo}>
           <div>
             {/* status */}
           </div>{/* Status Wrapper */}
-          <ol className='pl-8'>
+          <ol className={style.orderedList}>
             {/* TODO */}
           </ol>{/* Ordered List */}
         </div>{/* Status & Todo div */}
