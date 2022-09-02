ad912ff8fae861e6bc32f50517472f6c6f30ea52
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-09-02 17:03:42 +0300

    tamamdir

--- src/Game.js
+++ src/Game.js
@@ -8,21 +8,101 @@ const style = {
   statusAndTodo: "ml-5",
   orderedList:"pl-8"
 }
+
+// Calculate Winner fonksiyonu:
+// Amaç: tüm kareleri kontrol ederek oyunun kazananını bulmak.
+// Parametre: squares: karelerin dizisi.
+// Potansiyel olarak kazanılan adımları keşfetmek istenmektedir.
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
 // Game Class'ı:
 // App.js gibi düşünülebilir.
 // Component'lerin renderlandığı yer.
 
 class Game extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      xIsNext: true,
+    }
+  }
+
+  //* handleClick(i) fonksiyonu:
+  // Amaç: kareye tıklandığında o kareye X veya O karakteri yazılır.
+  // Parametre: i: karelerin indexi.
+  // if (calculateWinner(this.state.squares) || this.state.squares[i]) {
+  //   return;
+  // }
+  // bu yukardaki if komutu, eğer işleme giren kare daha önce kullanılmışsa veya oyunun kazananı varsa kareye tıklanamaz.
+  // değiştirilemez.
+  // else altında kareye tıklanıldığında kareye X veya O karakteri yazılır. Sırada kim varsa.
+  // Önemli!
+  // Immutable önemli bir kavramdır. Array'in kendisinin değiştirilmesi tavsiye edilmez. 
+  // Array'in kendisini değiştirmektense .slice() kullanılarak kopyalama yapılır.
+  // const squares = this.state.squares.slice(); 'de olduğu gibi.
+  // squares: squares diyerek kopyalanan dizide değişiklik yapılabilir ve ana diziye en son yapılan değişiklikler gönderilir.
+
+
+  handleClick(i) {
+    const history = this.state.history
+    const current = history[history.length - 1]
+    const squares = current.squares.slice()
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    } else {
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        history: history.concat([{
+        squares: squares,
+        }]),
+        xIsNext: !this.state.xIsNext,
+      })
+    }
+  }
+
   render() {
+    const history = this.state.history
+    const current = history[history.length - 1]
+    const winner = calculateWinner(current.squares)
+    let status;
+    if (winner) {
+      status = "Winner: " + winner
+    } else if (current.squares.every(square => square !== null)) {
+      status = "No Winner"
+    } else {
+      status = "Next Player: " + (this.state.xIsNext ? 'X' : 'O')
+    }
     return (
       <div className={style.main}>
         <div className={style.board}>
-          <Board />
+          <div>
+            {status}
+            <Board squares={current.squares} onClick={(i) => this.handleClick(i)}/>
+          </div>
         </div>{/* Board div */}
         <div className={style.statusAndTodo}>
-          <div>
-            {/* status */}
-          </div>{/* Status Wrapper */}
           <ol className={style.orderedList}>
             {/* TODO */}
           </ol>{/* Ordered List */}
--- src/components/Board.js
+++ src/components/Board.js
@@ -14,30 +14,6 @@ import { motion } from 'framer-motion';
 
 */
 
-// Calculate Winner fonksiyonu:
-// Amaç: tüm kareleri kontrol ederek oyunun kazananını bulmak.
-// Parametre: squares: karelerin dizisi.
-// Potansiyel olarak kazanılan adımları keşfetmek istenmektedir.
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
-    }
-  }
-  return null
-}
 
 // Board Class'ı:
 // constructor()'ı olan bir sınıf super(props) almak zorunda.
@@ -46,49 +22,18 @@ function calculateWinner(squares) {
 // xIsNext : X karakteri ilk oynanacak.(true dönmüş) / false olsaydı O karakteri ilk oynanacaktı.
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    }
-  }
-
-  // handleClick(i) fonksiyonu:
-  // Amaç: kareye tıklandığında o kareye X veya O karakteri yazılır.
-  // Parametre: i: karelerin indexi.
-  // if (calculateWinner(this.state.squares) || this.state.squares[i]) {
-  //   return;
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     squares: Array(9).fill(null),
+  //     xIsNext: true,
+  //   }
   // }
-  // bu yukardaki if komutu, eğer işleme giren kare daha önce kullanılmışsa veya oyunun kazananı varsa kareye tıklanamaz.
-  // değiştirilemez.
-  // else altında kareye tıklanıldığında kareye X veya O karakteri yazılır. Sırada kim varsa.
-  // Önemli!
-  // Immutable önemli bir kavramdır. Array'in kendisinin değiştirilmesi tavsiye edilmez. 
-  // Array'in kendisini değiştirmektense .slice() kullanılarak kopyalama yapılır.
-  // const squares = this.state.squares.slice(); 'de olduğu gibi.
-  // squares: squares diyerek kopyalanan dizide değişiklik yapılabilir ve ana diziye en son yapılan değişiklikler gönderilir.
-
-
-  handleClick(i) {
-    const squares = this.state.squares.slice()
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    } else {
-      squares[i] = this.state.xIsNext ? 'X' : 'O';
-      this.setState({
-        squares: squares,
-        xIsNext: !this.state.xIsNext,
-      })
-    }
-  }
-
-
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     )
   }
@@ -107,16 +52,6 @@ class Board extends React.Component {
   
   */
   render() {
-    const winner = calculateWinner(this.state.squares)
-    let status;
-    if (winner) {
-      status = "Winner: " + winner
-    } else if (this.state.squares.every(square => square !== null)) {
-      status = "No Winner"
-    } else {
-      status = "Next Player: " + (this.state.xIsNext ? 'X' : 'O')
-    }
-
 
     // Framer Motion Part
     // Açılış kısmındaki animasyon.
@@ -151,26 +86,26 @@ class Board extends React.Component {
         }
       }
     }
-    const text = {
-      hidden: { y: -150, opacity: 0 },
-      visible: {
-        y: 0,
-        opacity: 1,
-        transition: {
-          duration: 1,
-        }
-      }
-    }
+    // const text = {
+    //   hidden: { y: -150, opacity: 0 },
+    //   visible: {
+    //     y: 0,
+    //     opacity: 1,
+    //     transition: {
+    //       duration: 1,
+    //     }
+    //   }
+    // }
     // Framer Motion Part
 
     return (
       <div>
-        <motion.div
+        {/* <motion.div
           initial="hidden"
           animate="visible"
           variants={text}
-          className="mb-2.5 hover:underline">{status}
-        </motion.div>{/* Status Div */}
+          className="mb-2.5 hover:underline">
+        </motion.div> Status Div */}
 
         <motion.div
           initial="hidden"
